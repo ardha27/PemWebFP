e316833a4dcfdd8532445930ecdd41c08cd5c9b6
Ardha <ardhasemaranatha@gmail.com>
2020-12-19 13:12:58 +0800

    init

---
+++ config/connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "online_shop";
+// Create connection
+define("BASE_URL", "http://localhost/online_shop/");
+$conn = new mysqli($servername, $username, $password,$database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+?>
---
+++ kategori_delete.php
@@ -0,0 +1,10 @@
+<?php
+    require"includes/header.php";
+    $id = $_GET['id'];
+
+    $aVar = new mysqli('localhost', 'root', '','online_shop');
+    $query = mysqli_query($aVar, "delete from kategori where id_kategori ='$id'");
+    if($query) {
+        echo "<meta http-equiv='refresh' content='0, url=".BASE_URL."kategori.php'/>";
+    }
+?>
